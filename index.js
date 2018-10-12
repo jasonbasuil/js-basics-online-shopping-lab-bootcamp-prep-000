15f753ec00e4ed5f3f2efbfa635b8e2ddb2ee6d6
Jason Basuil <basuilj@gmail.com>
2018-10-12 03:42:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,15 +10,53 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var newItem = {};
+  newItem.itemName = item;
+  newItem.itemPrice = Math.floor(Math.random() * 101);
+  cart.push(newItem);
+  console.log(`${item} has been added to your cart.`);
 }
 
 function viewCart() {
-  // write your code here
+  itemNames = [];
+  itemPrices = [];
+  string1 = 'In your cart, you have '
+  string2 = []
+  string3 = 'and'
+  string4 = []
+  singleSentence = 
+  
+  //iterate through cart Objects, and return itemName & itemPrice into separate arrays
+  //use a loop to iterate through the two arrays with conditionals based on the number of items 
+  
+  //for 3+ items
+  for (i = 0; i <= itemNames.length; i++){
+    if (itemNames.length === 1){
+    return (`In your cart, you have ${itemNames[0]} at $${itemPrices[0]}.`);
+  }
+    else if (itemNames.length ===2){
+      return (`In your cart, you have ${itemNames[0]} at $${itemPrices[0]}, and ${itemNames[1]} at $${itemPrices[1]}.`)}
+    else{
+      ...
+      
+      return (`In your cart, you have $)
+    }
+    
+  
+  
+  
+
+  for (itemNumber = 0; itemNumber <= itemNumber.length; itemNumber++){
+    currentItem.push(cart[itemName]);
+    currentPrice.push(cart[itemPrice]);
+  
+}  
+  
+  
 }
 
 function total() {
-  // write your code here
+  
 }
 
 function removeFromCart(item) {
